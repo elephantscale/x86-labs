6c717c559201f7a845c079e2c85a1117964d8b28
Tim Fox <tim@elephantscale.com>
2021-06-10 08:35:56 -0400

    Removed solution

--- chapter-12/exercise-14.s
+++ chapter-12/exercise-14.s
@@ -4,12 +4,5 @@
 	.type times10, @function
 	
 times10:
-	mov	ecx, 10		# loop 10 times
-loop:
-	push	rdi		# preserve rdi (string)
-	push	rcx		# preserve rcx (counter)
-	call	puts@plt
-	pop	rcx		# resture counter
-	pop	rdi		# restore string pointer
-	loop	loop
+	/* TODO: Complete this exercise. */
 	ret
